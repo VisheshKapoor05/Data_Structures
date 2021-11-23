42b7e91915206f15a86e468f44e7528de765d25d
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-23 14:49:53 +0530

    UC9: added deleteNode method to delete a node and added size method to get size of linkedList

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -8,10 +8,15 @@ public class LinkedListDemo{
 		linkedList.insertFirst(70);
 		linkedList.insertFirst(30);
 		linkedList.insertFirst(56);
-		
 		linkedList.insertAfter(40, 30);
-		
 		linkedList.displayNodesData();
 		
+		System.out.println(" ");
+		System.out.println("Deleting 40:");
+		linkedList.deleteNode(40);
+		linkedList.displayNodesData();
+		
+		System.out.println(" ");
+		System.out.println("Size: " +linkedList.size());
 	}
 }
